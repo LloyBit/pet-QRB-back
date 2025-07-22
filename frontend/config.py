46d5fdb8a996035aa3fa821abeaaa65640cd748d
Dmitriy <Krivopusk.mitya@gmail.com>
2025-07-22 23:47:06 +0300

    service split

--- frontend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# QR_SERVICE_URL =
-# TOKEN_SERVICE_URL = 
--- run_all.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-s1 = subprocess.Popen(["python", "backend/app/main.py"])
-s2 = subprocess.Popen(["python", "frontend/bot/bot.py"])
-
-
-s1.wait()
-s2.wait()
